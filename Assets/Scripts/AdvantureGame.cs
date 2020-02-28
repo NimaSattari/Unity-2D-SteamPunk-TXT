53b64ee3e4804e55b0d87ffadac3e8d4db856403
NimaSattari <54527075+NimaSattari@users.noreply.github.com>
2020-02-28 13:23:14 +0330

    Initial commit

---
+++ Assets/Scripts/AdvantureGame.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdvantureGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+    State state;
+
+    void Start()
+    {
+        state = startingState;
+        textComponent.text = state.GetStateStory();
+    }
+
+
+    void Update()
+    {
+        ManageState();
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+    private void ManageState()
+    {
+        var nextStates = state.GetNextStates();
+
+        for(int i = 0; i < nextStates.Length; i++)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1 + i))
+            {
+                state = nextStates[i];
+            }
+        }
+
+        textComponent.text = state.GetStateStory();
+    }
+}
